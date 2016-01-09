8274ce5e9e4ab94728c94d0f664fb152b279852b
poiuyt <poiuyt@poiuyt-X550CA>
2016-01-09 09:04:57 +0700

    make action bar

---
+++ Alarm/app/src/main/java/poiuyt/alarm/activities/AlarmService.java
@@ -0,0 +1,35 @@
+package poiuyt.alarm.activities;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import poiuyt.alarm.data.AlarmApart;
+import poiuyt.alarm.helpers.DataHelper;
+import poiuyt.alarm.utils.LogUtils;
+
+
+public class AlarmService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    public static void startAlarmService(Context context, Calendar calendar) {
+        LogUtils.d("Starting alarm service: calendar: " + calendar.toString());
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intentOfAlarm = new Intent(context, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentOfAlarm, 0);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+
+}
